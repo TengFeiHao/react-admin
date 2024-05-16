d5cfdccfa4545c6d83a5251d5ed27b14ba248981
haotengfei <haotengfei@zhidoushi.cn>
2024-05-16 10:16:45 +0800

    fix(useState): 原理解析

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,10 @@ import App from './App.tsx'
 import '@/assets/style/normalize.scss'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
+  /**
+   * StrictMode 严格模式在生产环境下不生效，因此它不会降低应用程序的速度
+   * React 提供了 “严格模式”，在严格模式下开发时，它将会调用每个组件函数两次。通过重复调用组件函数，严格模式有助于找到违反这些规则的组件。
+   */
   <React.StrictMode>
     <App />
   </React.StrictMode>,
